40f761460dd43462c230e788c2dfd35c418a2f02
sh-yoshimura <sh-yoshimura@kentem.co.jp>
2024-02-28 19:15:33 +0900

    dangerインストール

---
+++ dangerfile.ts
@@ -0,0 +1,11 @@
+import { danger, warn } from 'danger'
+
+const hasModifiedPackageJson =
+  danger.git.modified_files.includes('package.json' )
+const hasModifiedPackageLockJson =
+  danger.git.modified_files.includes('package.json' )
+// package.jsonに修正があるが、package-lock.jsonにない
+if (hasModifiedPackageJson && !hasModifiedPackageLockJson)
+  warn(
+    'package.jsonが修正されていますが、package-lock.jsonが修正されていないようです。',
+  )
